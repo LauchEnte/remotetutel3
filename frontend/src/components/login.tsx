1be8b0c9428631aeab81ce1bde70557ad12195de
LauchEnte <bock-david@outlook.de>
2025-08-21 21:23:27 +0200

    nothing mentionable

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,7 +1,7 @@
 import {connect} from './websocket.ts'
 import tutel from '/tutel.ico'
 
-export default function LoginPage({setWebsocket, errorStuff} : {setWebsocket: Function, errorStuff?: {message?: string, setMessage: Function}}){
+export default function LoginPage({setWebsocket, errorStuff} : {setWebsocket: any, errorStuff?: {message?: string, setMessage: Function}}){
     
     function onSubmit(e: any){
         const password = e.target.value
--- setup.bat
+++ setup.bat
@@ -5,7 +5,7 @@ echo building frontend
 call npm run build
 cd ..
 echo creating python virtual environment
-call py -m venv venv
+call python -m venv venv
 call venv/scripts/activate
 echo installing python dependencies
 call pip install -r requirements.txt
